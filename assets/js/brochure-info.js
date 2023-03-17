ff7a2eaf37e69f20f1c23adccfca1cb60eca8081
Amaaz Ahmad <curriculum@knowledge.tech>
2023-03-17 17:19:17 +0500

    brochure form firebaseapp implemented

--- assets/js/brochure-info.js
+++ assets/js/brochure-info.js
@@ -1,3 +1,18 @@
+const firebaseConfig2 = {
+     apiKey: "AIzaSyBzsNcUNJbF-F2-pcvbveu176Lt54Vlj58",
+     authDomain: "ks-web-brochure.firebaseapp.com",
+     databaseURL: "https://ks-web-brochure-default-rtdb.asia-southeast1.firebasedatabase.app",
+     projectId: "ks-web-brochure",
+     storageBucket: "ks-web-brochure.appspot.com",
+     messagingSenderId: "947151666437",
+     appId: "1:947151666437:web:01912bb405a8d262bbcb6c"
+};
+
+
+// Initialize Firebase
+
+const app_brochure = firebase.initializeApp(firebaseConfig2, 'second');
+
 var thisForm = document.querySelector('#brochure-form');
 
 thisForm.addEventListener('submit', function (e){
@@ -15,7 +30,8 @@ thisForm.addEventListener('submit', function (e){
           });
           data['timestamp'] = new Date().toLocaleString()
 
-          console.log("DATA: ", data)
+          var database = app_brochure.database();
+          const newRef = database.ref('brochure-form-responses').push(data);
 
         
      }
